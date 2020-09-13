a853451e033215d92f0c7b191fe0cf23f4af731a
Angela Wang <for_register@yeah.net>
2020-09-13 20:28:20 +0800

    Update common.js

--- javascript/common/common.js
+++ javascript/common/common.js
@@ -4,3 +4,21 @@ let shop = cart[shopid] = (cart[shopid] || {})
 //数据拷贝
 // Object.assign方法实行的是浅拷贝，而不是深拷贝。也就是说，如果源对象某个属性的值是对象，那么目标对象拷贝得到的是这个对象的引用。
 Object.assign({}, state.orderParam, newParam);
+
+// js中判断变量不为空或null
+//相当于判断content=""、content=null、content = undefined、content=0
+if (!content) {
+    alert("请输出内容！");
+    return;
+}
+
+
+// 判断字符串为空的方法
+var strings = '';
+if (string.length === 0) {}
+
+//Boolean类型设相反的值
+this.value = !this.value
+
+//字符串false转为Boolean类型
+JSON.parse("false")
